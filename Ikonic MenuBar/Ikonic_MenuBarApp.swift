a3f3416ae86e27ae0f326b2b9268ed0c254d1f28
BXAMRA <jassbhamra@outlook.com>
2025-08-29 12:40:58 +0530

    Initial Commit

---
+++ Ikonic MenuBar/Ikonic_MenuBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ikonic_MenuBarApp.swift
+//  Ikonic MenuBar
+//
+//  Created by Jass Bhamra on 2025-08-29.
+//
+
+import SwiftUI
+
+@main
+struct Ikonic_MenuBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
